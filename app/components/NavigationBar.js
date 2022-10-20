2a8fd7222ef6402003174ecbce671dbe1ac40d33
Remy Wood <99709675+remwood@users.noreply.github.com>
2022-10-20 16:32:56 -0700

    Create NavigationBar component

---
+++ app/components/NavigationBar.js
@@ -0,0 +1,30 @@
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import menuLightIcon from "../../assets/Icons/menu_light.png";
+import sunIcon from "../../assets/Icons/sun.png";
+
+export default function NavigationBar() {
+  return (
+    <View style={styles.container}>
+      <Image source={menuLightIcon} style={styles.button} />
+      <Text style={styles.title}>ensom</Text>
+      <Image source={sunIcon} style={styles.button} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: Platform.OS === "android" ? 54 : 41,
+    paddingHorizontal: 16,
+  },
+  button: {
+    height: Platform.OS === "android" ? 48 : 32,
+    width: Platform.OS === "android" ? 48 : 32,
+  },
+  title: {
+    flexGrow: 1,
+    fontSize: 32,
+    textAlign: "center",
+  },
+});
